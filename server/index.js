cdb6127063a3c04a2b20a8bce38404f213b24e6e
Godfrey Bosire <godfreybosire@Godfreys-MacBook-Pro.local>
2023-11-02 17:53:23 +0300

    #

--- server/index.js
+++ server/index.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 const authRoutes = require("./routes/auth.js");
 
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 5001;
 
 require('dotenv').config();
 
